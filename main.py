46b4c82ce67c1dcf99242c3e971c744d5dfc40b8
VanshikaPal739 <vanshikapal44@gmail.com>
2024-08-30 14:54:06 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Hello world")
+print("Vanshika ")
+print("my age is 20", "print")
+print(30)
+print(9+20)
+
+
+name="vanshika"
+age= 20
+price= 34.6
+print(type(name))
+print(type(age))
+print(type(price))
